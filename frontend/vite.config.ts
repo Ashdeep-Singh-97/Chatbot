adfab1df30c215aec04a3ac6a72711ca2f0ebfb9
Ashdeep-Singh-97 <ashdeepsingh1997@gmail.com>
2024-10-23 03:40:01 +0530

    vercel.json and vite.config updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     // Default should work for local development
     host: true, // or just omit this line
