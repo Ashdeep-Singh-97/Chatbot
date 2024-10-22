ae6c44c9ebca98148ba9fcafce7c64132912bf28
Ashdeep-Singh-97 <ashdeepsingh1997@gmail.com>
2024-10-23 02:51:51 +0530

    added dockerfiles, svg, vercel.json and CI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Default should work for local development
+    host: true, // or just omit this line
+    port: 5173,
+  },
 })
