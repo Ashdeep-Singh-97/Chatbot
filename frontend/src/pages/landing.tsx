86ab90f8e41d04d8687479d27ea0b568f1707e8c
Ashdeep-Singh-97 <ashdeepsingh1997@gmail.com>
2024-11-11 02:51:15 +0530

    Added credentials

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -37,6 +37,9 @@ const landing: React.FC = () => {
           <div className='text-6xl font-stratford font-light mb-12'>
             With <span className="font-semibold">Claude</span>
           </div>
+          <div className='font-stratford font-light mb-12'>
+            (email: bote@gmail.com | password: 123)
+          </div>
         </div>
 
         <div style={{ backgroundColor: 'rgb(223, 219, 205)' }} className="w-full max-w-md p-5 shadow-2xl rounded-lg component opacity-100">
@@ -52,22 +55,7 @@ const landing: React.FC = () => {
             />
           )}
         </div>
-        
-        {/* <div className='page-down absolute bottom-44 flex items-center mt-6 px-4 border border-gray-600 py-3 rounded-lg bg-white'>
-          <div className="px-1 py-0.5 border border-gray-600 text-white rounded-xl mr-2 bg-blue-700">
-            new
-          </div>
-          <span className="text-gray-600">Meet Claude 3.5 Sonnet, our latest model. Hope so that you will enjoy using it.</span>
-        </div>
 
-        <div className='page-down text-center absolute bottom-24 text-2xl font-stratford'>
-          Claude is a next generation AI assistant built for work <br></br>
-          and trained to be safe, accurate and secure.
-        </div>
-
-        <div className='page-down absolute bottom-12 font-mono'>
-          By Arsh
-        </div> */}
       </div>
     </div>
   );
